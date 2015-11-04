f2b922a2d04f336c03c1939928f62758da0f02dc
josoi <jnelson1@stevens.edu>
2015-11-04 17:06:11 -0500

    debate creation urls

--- debates/urls.py
+++ debates/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.front_page, name='front_page'),
+    url(r'^create_debate$', views.create_debate, name='create_debate'),
+    url(r'^created_dummy$', views.created_dummy, name='created_dummy'),
 ]
 
 
