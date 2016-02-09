2070bdbcab8a0cf7318a802d88524be68321c4b3
josoi <jnelson1@stevens.edu>
2016-02-09 13:46:51 -0500

    calculates random debates to display
    lists all debates in a random order and grabs only the first 10

--- debates/views.py
+++ debates/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Debate, Category, ChatMessage
 from django.http import Http404
+import random
 
 # Create your views here.
 
 def front_page(request, redirect_message=None):
 
     categories = Category.objects.all()
+    debates = Debate.objects.order_by('?')
+    to_display = debates[:10]
+    #categories = categories.first()
     #return HttpResponse(','.join([a.topic for a in categories[1].debate_set.all()]))
-    context = {"categories":categories, "redirect_message":redirect_message}
+    context = {"categories":categories, "to_display":to_display, "redirect_message":redirect_message}
     print (context)
     return render(request, "front.html", context)
 
